3e92066654674191a15d9a50414b1c6f1c514024
rebecaaras <rebecaaraujo2013@gmail.com>
2024-09-04 15:41:16 -0300

    fix: Refactor for loops that consume json data replacing them with the map function

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
 //imports
 const express = require('express');
 const fs = require('fs');
-const app = express();
-app.use(express.json());
 const Joi = require('joi');
+const app = express();
 
-const schema = Joi.object({ //schema for validation
+app.use(express.json());
+
+//schema for validation
+const schema = Joi.object({
     name: Joi.string().min(3).required()
 });
 
-//data import
+//Data import
 const artistData = JSON.parse(fs.readFileSync('data/artist.json', 'utf-8'))
 const albumsData = JSON.parse(fs.readFileSync('data/albums.json', 'utf-8'))
 const topTracksData = JSON.parse(fs.readFileSync('data/top-tracks.json', 'utf-8'))
 
-const courses = [
-    {id: 1, name: 'course1'},
-    {id: 2, name: 'course2'},
-    {id: 3, name: 'course3'},
-];
+//Array with all albums
+const albums = albumsData.items.map((item, index) => ({
+    id: index + 1,
+    name: item.name,
+    cover: item.images[0].url,
+    release_date: item.release_date,
+    total_tracks: item.total_tracks,
+}));
 
-//GET METHODS
+//Array with top tracks
+const topTracks = topTracksData.tracks.map(track => ({
+    name: track.name,
+    album: track.album.name,
+    release_date: track.album.release_date,
+    popularity: track.popularity
+}));
+
 app.get('/api/artist/hozier', (req, res) => {
     artist = {name: artistData.name, 
         followers: artistData.followers.total,
@@ -30,44 +42,27 @@ app.get('/api/artist/hozier', (req, res) => {
     res.send(artist);
 });
 
-//Fazer dicionário com albums e informações do album
-let albums = {}
-     for (i = 0; i < albumsData.items.length; i++){
-        albums[parseInt(i + 1)] = {name: albumsData.items[i].name, 
-            cover: albumsData.items[i].images[0].url,
-            release_date: albumsData.items[i].release_date,
-            total_tracks: albumsData.items[i].total_tracks};
-     }
-
 app.get('/api/artist/hozier/albums', (req, res) => {
     res.send(albums)
 });
 
 app.get('/api/artist/hozier/albums/:id', (req, res) => {
     //This code is a bit unclear to me...
-    let album = albums[req.params.id]//.find(a => a === parseInt(req.params.id));
+    index = parseInt(req.params.id)
+    let album = albums[index]
     if (!album){
         res.status(404).send('The album with the given id was not found.');
     } else {
         res.send(album);
     }
-    
 });
 
-let topTracks = [] //topTracksData//[]
-for (i = 0; i < topTracksData.tracks.length; i++){
-    topTracks[i] = {name: topTracksData.tracks[i].name,
-        album: topTracksData.tracks[i].album.name,
-        release_date: topTracksData.tracks[i].album.release_date,
-        popularity: topTracksData.tracks[i].popularity,     
-        }
- }
-
- app.get('/api/artist/hozier/top-tracks', (req, res) => {
+app.get('/api/artist/hozier/top-tracks', (req, res) => {
     res.send(topTracks)
 });
 
 myFavouriteTracks = [];
+//POST favourite track
 app.post('/api/artist/hozier/my-favourite-tracks', (req, res) => {
     const result = schema.validate(req.body);
     if (result.error){
@@ -89,8 +84,10 @@ app.get('/api/artist/hozier/my-favourite-tracks', (req, res) => {
     res.send(myFavouriteTracks)
 });
 
+//DELETE favourite tracks
 app.delete('/api/artist/hozier/my-favourite-tracks/:id', (req, res) => {
-    const trackToDelete = myFavouriteTracks.find(t => t.id === parseInt(req.params.id))
+    index = parseInt(req.params.id)
+    const trackToDelete = myFavouriteTracks.find(t => t.id === index)
     if (!trackToDelete){
         res.status(404).send('The track with the given id was not found.');
     } else {
